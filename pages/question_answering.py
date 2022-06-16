1c7d945743c09d37a8fe59701d88babca9172d98
echan <echan@nfa.futures.org>
2022-06-16 13:40:46 +0000

    added no answer found to question answering

--- pages/question_answering.py
+++ pages/question_answering.py
@@ -38,7 +38,10 @@ def app():
     with st.spinner('Generating answer . . .'):
         if button and sentence:
             answers = qa(question = question, context = sentence)
-            st.write(answers['answer'])
+            if answers['score'] >= 0.01: # threshold
+                st.write(answers['answer'])
+            else:
+                st.write('No answer found.')
 
     # Hide Streamlit branding
     hide_st_style = """
